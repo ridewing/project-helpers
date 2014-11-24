af9c83ba9b9553f319a2be2d0a2fb89ec36fdfb3
Nicklas Ridewing <nicklas@x-com.se>
2014-11-24 19:06:13 +0100

    First working version

--- index.js
+++ index.js
@@ -18,34 +18,14 @@
 		del			= require('del'),
 		notify		= require("gulp-notify"),
 		concat		= require("gulp-concat"),
-		sourcemaps	= require('gulp-sourcemaps');
+		sourcemaps	= require('gulp-sourcemaps'),
+		newer 		= require('gulp-newer'),
+		uglify 		= require('gulp-uglify'),
+		path		= require('path');
 	
-	var ProjectHelpers = function (settings) {
-		var self = this;
-
-		self.typescript = settings.typescript || false;
-		
-		self.sourcePath		= 'source/';
-		self.buildPath		= 'build/';
-		self.componentsPath	= 'components/';
-
-		self.directories = {
-			styles 	: '/styles',
-			scripts : '/scripts',
-			images 	: '/images/'
-		};
-
-		self.paths = {
-			components	: self.sourcePath + 'components/**/*.js',
-			scripts		: self.sourcePath + 'scripts/app/**/*.' + (self.typescript)?'.ts':'.js',
-			images		: self.sourcePath + 'images/**/*',
-			styles 		: self.sourcePath + 'styles/**/*.less'
-		};
-		
-		self.components = [];
-		
-		self.setup();
-	}
+	var globals = {};
+	
+	var ProjectHelpers = function () {}
 
 	ProjectHelpers.prototype.setSourcePath = function (path) {
 		this.sourcePath = path;
@@ -58,11 +38,38 @@
 	ProjectHelpers.prototype.registerComponent = function (name, path) {
 		this.components.push({
 			'name' : name,
-			'path' : this.componentsPath + '/' + name + '/' + path;
+			'path' : this.componentsPath + name + '/' + path
 		});
 	}
 	
-	ProjectHelpers.prototype.setup = function () {
+	ProjectHelpers.prototype.setup = function (settings) {
+		
+		var self = this;
+		
+		this.name 			= settings.name 			|| 'Unnamed Project-Helpers';
+		this.sourcePath		= settings.sourcePath  		|| 'source/';
+		this.buildPath		= settings.buildPath  		|| 'build/';
+		this.componentsPath	= settings.componentsPath  	|| 'components/';
+
+		globals.title = this.name;
+		
+		this.directories = {
+			styles 		: settings.stylesFolder		|| 'styles',
+			scripts 	: settings.scriptsFolder	|| 'scripts',
+			images 		: settings.imagesFolder 	|| 'images'
+		};
+
+		this.paths = {
+			scripts		: this.sourcePath + this.directories.scripts + '/**/*' + ((this.typescript)?'.ts':'.js'),
+			images		: this.sourcePath + this.directories.images + '/**/*',
+			styles 		: this.sourcePath + this.directories.styles + '/**/*.less'
+		};
+		
+		this.components = [];
+		
+		this.typescript = settings.typescript 	|| false;
+		this.debug 		= settings.debug 		|| false;
+		
 		if(this.typescript)
 		{
 			var typescriptProject = typescript.createProject({
@@ -72,12 +79,12 @@
 			});
 			
 			gulp.task('ph-typescript', function () {
-				var ts = gulp.src(this.paths.scripts)
+				var ts = gulp.src(self.paths.scripts)
 					.pipe(typescript(typescriptProject));
 
 				return ts.js
 					.pipe(concat('main.min.js'))
-					.pipe(gulp.dest(this.buildPath  + 'scripts/'))
+					.pipe(gulp.dest(self.buildPath + self.directories.scripts))
 					.pipe(alert("Typescript compiled successfully"));
 			});
 		}
@@ -89,32 +96,87 @@
 			}
 			else
 			{	
-				gulp.src(this.paths.scripts)
+				gulp.src(self.paths.scripts)
 					.pipe(sourcemaps.init())
 					.pipe(concat('main.min.js'))
+					.pipe(uglify())
 					.pipe(sourcemaps.write())
-					.pipe(gulp.dest(this.buildPath + 'scripts'))
+					.pipe(gulp.dest(self.buildPath + self.directories.scripts))
 					.pipe(alert("Script task completed"));
 			}
 		});
 		
-		gulp.task('ph-styles', function () {
-			var style = less().on("error", error("Error compiling LESS")):
+		gulp.task('ph-components', function()
+		{
+			var components = [];
 			
-			gulp.src(this.paths.styles)
+			for(var key in self.components)
+			{
+				components.push(self.components[key].path);
+				
+				if(this.debug)
+				{
+					console.log('Adding component:', self.components[key].name, '('+self.components[key].path+')');
+				}
+			}
+			
+			gulp.src(components)
+					.pipe(sourcemaps.init())
+					.pipe(concat('components.min.js'))
+					.pipe(sourcemaps.write())
+					.pipe(gulp.dest(self.buildPath + self.directories.scripts))
+					.pipe(alert("Components task completed"));
+		});
+		
+		gulp.task('ph-images', function(event)
+		{
+			gulp.src(self.paths.images)
+				.pipe(newer(self.buildPath + 'images'))
+				.pipe(imagemin({optimizationLevel: 5}))
+				.pipe(gulp.dest(self.buildPath + self.directories.images))
+				.pipe(notify("Image task completed"));
+		});
+		
+		gulp.task('ph-styles', function () {
+			var style = less().on("error", error("Error compiling LESS"));
+			
+			gulp.src(self.paths.styles)
 				.pipe(style)
-				.pipe(gulp.dest(this.buildPath + 'style/'))
+				.pipe(gulp.dest(self.buildPath + self.directories.styles))
 				.pipe(alert("Less task completed"));
 		});
 		
-		gulp.task('ph-watch', function () {
-			gulp.watch(this.paths.components, 	['ph-components']);
-			gulp.watch(this.paths.scripts, 		['ph-scripts']);
-			gulp.watch(this.paths.images, 		['ph-images']);
-			gulp.watch(this.paths.styles, 		['ph-styles']);
+		gulp.task('ph-clean', function () {
+			self.clean();
 		});
 	}
 	
+	ProjectHelpers.prototype.watch = function () {
+		
+		var components = [];
+			
+		for(var key in this.components)
+		{
+			components.push(this.components[key].path);
+		}
+		
+		gulp.watch(components, 				['ph-components']);
+		gulp.watch(this.paths.scripts, 		['ph-scripts']);
+		gulp.watch(this.paths.images, 		['ph-images']);
+		gulp.watch(this.paths.styles, 		['ph-styles']);
+	}
+	
+	ProjectHelpers.prototype.clean = function () {
+		del([
+			this.buildPath + 'styles/**/*', 
+			this.buildPath + 'scripts/**/*', 
+			this.buildPath + 'images/**/*'
+		], {force : true});
+	}
+	
+	ProjectHelpers.prototype.build = function () {
+		gulp.start(['ph-styles', 'ph-scripts', 'ph-components', 'ph-images']);
+	}
 	
 	/*
 	 * Private functions
@@ -122,17 +184,22 @@
 
 	function alert (msg) {
 		return notify({
-			message: msg,
-        	title: "Project Helpers"
+        	title	: msg,
+			icon	: false,
+        	subtitle: globals.title
 		})
 	}
 	
 	function error (msg) {
-		notify.onError({
+		return notify.onError({
+			title	: msg,
 			message : "<%= error.message %>",
-			title 	: msg
+			subtitle: globals.title,
+			icon	: false
 		})
 	}
+	
+	module.exports = new ProjectHelpers;
 	
 })();
 
